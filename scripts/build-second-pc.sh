3fb82c85c6f881a173f60fa609aec4a0b3def4b8
0utsights <outsightszs@outlook.com>
2026-08-25 03:19:16 -0400

    Harden second PC deployment packaging

--- scripts/build-second-pc.sh
+++ scripts/build-second-pc.sh
@@ -25,7 +25,7 @@ docker run --rm --dns "$build_dns" \
   -v "$repo_dir/collector:/src:ro" \
   -v "$repo_dir/collector/.second-pc-build:/out" \
   node:22-alpine \
-  sh -c 'mkdir /tmp/collector && cp -R /src/package.json /src/package-lock.json /src/tsconfig.json /src/src /tmp/collector/ && cd /tmp/collector && npm ci && npm test && npm prune --omit=dev && cp package.json package-lock.json /out/ && cp -R node_modules dist /out/'
+  sh -c 'mkdir /tmp/collector && cp -R /src/package.json /src/package-lock.json /src/tsconfig.json /src/src /tmp/collector/ && cd /tmp/collector && npm ci && npm test && npm prune --omit=dev && rm -rf /out/node_modules /out/dist && cp package.json package-lock.json /out/ && cp -R node_modules dist /out/'
 
 cd "$repo_dir"
 docker compose -f compose.yaml -f compose.second-pc.yaml build auction-server order-collector
